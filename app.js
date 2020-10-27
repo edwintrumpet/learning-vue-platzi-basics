a67ef2442cd2efaf56bfabab7da818931b8c07ad
Edwin Garcia <spark.com.co@gmail.com>
2020-10-27 18:34:19 -0500

    Clases en tiempo real

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ new Vue({
       name: 'Bitcoin',
       img: 'https://cryptologos.cc/logos/bitcoin-btc-logo.png',
       changePercent: 10,
-      prices: [8400, 7900, 8200, 9000, 9400, 10000, 10200],
+      price: 8400,
       pricesWithDays: [
         { day: 'Lunes', value: 8400 },
         { day: 'Martes', value: 7900 },
